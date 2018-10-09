d9d51e8c6fecc102fcc2de285b208ba92598120c
KilianBreton <kilian.breton.softk@gmail.com>
2018-10-09 12:03:51 +0200

    TEST UNITAIRE

--- pSportsPPE/tests/fr/rigaud/ArriveeTest.java
+++ pSportsPPE/tests/fr/rigaud/ArriveeTest.java
@@ -5,12 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ArriveeTest {
-
+	
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testCalculPrix() {
+		final double DELTA = 0.1;
+		Complexe comp = new Complexe(3,3,"yoman");
+		Arrivee ar = new Arrivee(comp,'M');
 		
-	//	Arrivee ar = new Arrivee();
-		assertEquals(0,0);
+		assertEquals(0.0,ar.calculPrix(14),DELTA);
+		assertEquals(0.5,ar.calculPrix(20),DELTA);
+		assertEquals(0.0,ar.calculPrix(20),DELTA);
+		assertEquals(0.0,ar.calculPrix(14),DELTA);
+		assertEquals(0.0,ar.calculPrix(14),DELTA);
+		assertEquals(0.0,ar.calculPrix(14),DELTA);
 	}
 
 }
