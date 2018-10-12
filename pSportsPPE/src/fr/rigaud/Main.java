8ccca89d850bb5dfc77dfec94842d5ec12f133f8
KilianBreton <kilian.breton.softk@gmail.com>
2018-10-12 15:08:07 +0200

    jfx

---
+++ pSportsPPE/src/fr/rigaud/Main.java
@@ -0,0 +1,27 @@
+package fr.rigaud;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/vue/view.fxml"));
+
+		    primaryStage.setScene(new Scene(root));
+		    primaryStage.setTitle("CFUN");
+		    primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
