c725f56e978beac8e10300e679be24562a391b9c
QUENOT <QUENOT@HOS2UC07.sio.fr>
2018-11-06 12:52:36 +0100

    Commit controleur et vue avec gestion affichage fit et muscu

--- pSportsPPE/src/vue/Controleur.java
+++ pSportsPPE/src/vue/Controleur.java
@@ -1,7 +1,12 @@
 package vue;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import fr.rigaud.*;
@@ -11,7 +16,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
@@ -33,15 +37,49 @@ public class Controleur {
 	@FXML
 	private Label l_main;
 	
+	//Info general
+	@FXML
+    private Label l_muscu_fm_comp;
+	@FXML
+    private Label l_muscu_fm_date;
+	@FXML
+    private Label l_muscu_fm_heure;
+	
+    @FXML
+    private Label l_fit_fm_comp;
+    @FXML
+    private Label l_fit_fm_date;
+    @FXML
+    private Label l_fit_fm_heure;
+	
+	
 	//Info Fitness/Muscu-----------------------------------------------
 	@FXML
-	private Label l_fm_t;
+	private Label l_muscu_fm_t;
 	@FXML
-	private Label l_fm_dispo;
+	private Label l_muscu_fm_dispo;
 	@FXML
-	private Label l_fm_ocup;
+	private Label l_muscu_fm_ocup;
 	@FXML
-	private Circle c_color;
+	private Label l_muscu_fm_etat;
+	@FXML
+    private Label l_muscu_fm_taux;
+	@FXML
+	private Circle c_muscu_color;
+	
+	@FXML
+    private Label l_fit_fm_t;
+    @FXML
+    private Label l_fit_fm_dispo;
+    @FXML
+    private Label l_fit_fm_ocup;
+    @FXML
+    private Label l_fit_fm_etat;
+
+    @FXML
+    private Label l_fit_fm_taux;
+    @FXML
+    private Circle c_fit_color;
 	
 	//Connexion--------------------------------------------------------
 	@FXML
@@ -50,14 +88,21 @@ public class Controleur {
 	private TextField tf_pass;
 	@FXML
 	private Button b_connection;
+	@FXML
+    private Tab tab_fit;
+	@FXML
+    private Tab tab_muscu;
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	private final String TITRE_MAIN = "Complexe ";
-	private final String TITRE_FITN = "Infos fitness";
-	private final String TITRE_MUSC = "Info Musculation";
+	private final String TITRE_MAIN = "Etat du Complexe : ";
+	private final String DATE = "date : ";
+	private final String HEURE = "heure : ";
+	private final String TITRE_FITN = "Infos Fitness";
+	private final String TITRE_MUSC = "Infos Musculation";
 	private final String NBPD = "Nombre de places occupées : ";
 	private final String NBPL = "Nombre de places libre : ";
+	private final String TXOCC = "Taux occ. : ";
 	
 	private Complexe comp;
 	
@@ -68,7 +113,7 @@ public class Controleur {
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public Controleur() {
-		comp = new Complexe(5,2,"test");
+		comp = new Complexe(5,4,"test");
 		Arrivee test = new Arrivee(comp,'M');
 		comp.entreeUsager(test);
 		comp.entreeUsager(test);
@@ -81,49 +126,80 @@ public class Controleur {
 	private void initialize()
 	{
 		// l_main.setText(TITRE_MAIN + comp.getNomComplexe());
-		 showFitOrMusc(true);
-		 
+		
 	}
 	
+	@FXML
+	private void changeToMuscu() {
+	    showFitOrMusc(false);
+	}
 	
-	
+	@FXML
+    private void changeToFit() {
+        showFitOrMusc(true);
+    }
 	
 	
 	
 	
 	private void showFitOrMusc(boolean fitn) {
 		double etat = 0;
+		double tauxOcc;
+		String date = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE).format(new Date());
+		String heure = new SimpleDateFormat("HH:mm", Locale.FRANCE).format(new Date());
+		
 		if(fitn) {
-			l_fm_t.setText(TITRE_FITN);
-			l_fm_dispo.setText(NBPL + comp.getNbPlacesRestantesFit());
+		    //tauxOcc = ;
+		    
+		    l_fit_fm_comp.setText(TITRE_MAIN + comp.getNomComplexe());
+		    l_fit_fm_date.setText(DATE + date);
+		    l_fit_fm_heure.setText(HEURE + heure);
+			l_fit_fm_t.setText(TITRE_FITN);
+			l_fit_fm_dispo.setText(NBPL + comp.getNbPlacesRestantesFit());
+			l_fit_fm_ocup.setText(NBPD + comp.getNbPlacesOccupeesFit());
+			l_fit_fm_taux.setText(TXOCC + tauxOcc);
 			etat = comp.etatFit();
 			
-			
+			switch(new ChoixCouleur(etat).getCouleur()) {
+            case vert:
+                c_fit_color.fillProperty().set(Paint.valueOf("green"));
+                break;
+            case orange:
+                c_fit_color.fillProperty().set(Paint.valueOf("orange"));
+                break;
+            case rouge:
+                c_fit_color.fillProperty().set(Paint.valueOf("red"));
+                break;
+			}
 		}
 		else {
-			l_fm_t.setText(TITRE_MUSC);
-			l_fm_dispo.setText(NBPL + comp.getNbPlacesRestantesMuscu());
+		    //tauxOcc = ;
+		    l_muscu_fm_comp.setText(TITRE_MAIN + comp.getNomComplexe());
+            l_muscu_fm_date.setText(DATE + date);
+            l_muscu_fm_heure.setText(HEURE + heure);
+			l_muscu_fm_t.setText(TITRE_MUSC);
+			l_muscu_fm_dispo.setText(NBPL + comp.getNbPlacesRestantesMuscu());
+			l_muscu_fm_ocup.setText(NBPD + comp.getNbPlacesOccupeesMuscu());
+			l_muscu_fm_taux.setText(TXOCC + tauxOcc);
 			etat = comp.etatMuscu();
-		}
-		
-
-		switch(new ChoixCouleur(etat).getCouleur()) {
-			case vert:
-				c_color.fillProperty().set(Paint.valueOf("green"));
-				break;
-			case orange:
-				c_color.fillProperty().set(Paint.valueOf("orange"));
-				break;
-			case rouge:
-				c_color.fillProperty().set(Paint.valueOf("red"));
-				break;
+			
+			switch(new ChoixCouleur(etat).getCouleur()) {
+            case vert:
+                c_muscu_color.fillProperty().set(Paint.valueOf("green"));
+                break;
+            case orange:
+                c_muscu_color.fillProperty().set(Paint.valueOf("orange"));
+                break;
+            case rouge:
+                c_muscu_color.fillProperty().set(Paint.valueOf("red"));
+                break;
+			}
 		}
 	}
 	
 	
 	@FXML
-	private void openWindow()
-	{
+	private void openWindow() {
 		try {
 			Stage primaryStage = new Stage();
 			Parent root = FXMLLoader.load(getClass().getResource("/vue/view.fxml"));
