19a249df5a4bc62e2db7fe6f05aff3240cbce15d
quenot <quenot@HOS2UC07.sio.fr>
2018-11-13 11:45:52 +0100

    Passage du controleur au controleur du gestionnaire et creation de la
    vue et du controleur de la vue genenral

---
+++ pSportsPPE/src/vue/ControleurConnexion.java
@@ -0,0 +1,16 @@
+package vue;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControleurConnexion {
+
+  //Connexion--------------------------------------------------------
+    @FXML
+    private TextField tf_login;
+    @FXML
+    private TextField tf_pass;
+    @FXML
+    private Button b_connection;
+}
