1af3f712a41e16e1f3ce4578433f837d5b1b3d1d
quenot <quenot@HOS2UC07.sio.fr>
2018-11-20 12:51:34 +0100

    suppresion des warnings du main

--- pSportsPPE/src/fr/rigaud/Main.java
+++ pSportsPPE/src/fr/rigaud/Main.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	private static Stage primaryStage;
